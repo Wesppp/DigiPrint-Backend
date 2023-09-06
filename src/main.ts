76dc5dcf6ee0a0fdfc75cbefc8899231abbd5685
Evgeny <pol96956@gmail.com>
2023-09-06 11:24:34 +0300

    add cookie parser

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { addSwagger } from './config';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   addSwagger(app);
+  app.use(cookieParser());
   await app.listen(process.env.DIGIPRINT_PORT);
   logger.log(`Server is running on ${await app.getUrl()}`, 'bootstrap');
 }
